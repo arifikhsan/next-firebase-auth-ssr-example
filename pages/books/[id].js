619c3b5b7cb3aba20cf3363c3f362ba81b376749
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-06-05 15:10:31 +0700

    update document

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -1,6 +1,6 @@
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import Link from 'next/link';
-import nookies from 'nookies'
+import nookies from 'nookies';
 import { useEffect, useState } from 'react';
 import { db } from '../../firebase/clientApp';
 
@@ -15,32 +15,56 @@ export function getServerSideProps(context) {
       props: {},
     };
   }
-  
-  return { props: {id: context.query.id} };
+
+  return { props: { id: context.query.id } };
 }
 
-export default function BookDetailPage({id}) {
+export default function BookDetailPage({ id }) {
   const [book, setBook] = useState(null);
-  
+  const [title, setTitle] = useState('');
+
   const watchBook = () => {
-    const bookRef = doc(db, 'books', id)
+    const bookRef = doc(db, 'books', id);
     onSnapshot(bookRef, (snapshot) => {
-      setBook(snapshot.data())
-    })
-  }
-  
+      setBook(snapshot.data());
+    });
+  };
+
   useEffect(() => {
     watchBook();
-  }, [])
+  }, []);
   
-  return <div>
-    <h1>BookDetailPage</h1>
-    <p>id: {id}</p>
-    <p>title: {book?.title}</p>
-    <p>createdAt: {book?.createdAt.toDate().toDateString()}</p>
-    
-    <Link href="/books">
-      Back to Books page
-    </Link>
-  </div>;
+  const onUpdate = (e) => {
+    e.preventDefault();
+    const bookRef = doc(db, 'books', id);
+    updateDoc(bookRef, { title });
+    setTitle('');
+  }
+
+  return (
+    <div>
+      <h1>BookDetailPage</h1>
+      <p>id: {id}</p>
+      <p>title: {book?.title}</p>
+      <p>createdAt: {book?.createdAt.toDate().toDateString()}</p>
+
+      <h2>
+        Edit Book
+        <form onSubmit={onUpdate}>
+          <input
+            type='text'
+            name='title'
+            placeholder='title'
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <button type='submit'>Update</button>
+        </form>
+      </h2>
+
+      <Link href='/books'>Back to Books page</Link>
+    </div>
+  );
 }
